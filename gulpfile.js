efc30f41cacbcb24b88ab12966466cd604f8fc16
LeeSinGOST <729076660@qq.com>
2023-07-06 12:20:01 +0800

    'push.bat'

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const replace = require('gulp-replace');
 gulp.task('templates', async() => {
 gulp.src('public/**/*.html')
-.pipe(replace('src="/../', 'src="https://fastly.jsdelivr.net/gh/LeeSinGOST/LeeSinGOST.github.io/'))
+.pipe(replace('cdn.jsdelivr.net', 'fastly.jsdelivr.net'))
 .pipe(gulp.dest('public/')), { overwrite: true };
 });
 gulp.task("default", gulp.parallel('templates'));
--- source/_posts/04 hexo 美化.md
+++ source/_posts/04 hexo 美化.md
@@ -1,9 +1,10 @@
 ---
 title: Hexo 美化
+abbrlink: 6888
 date: 2023-07-01 16:04:38
 updated: 2023-07-01 16:22:38
-categories: 
-tags: 
+categories:
+tags:
 ---
 # Hexo 美化
 [博客背景使用一图流 | QianChengGitの小森林](https://qianchenggit.github.io/2021/10/06/%E5%8D%9A%E5%AE%A2%E8%83%8C%E6%99%AF%E4%BD%BF%E7%94%A8%E4%B8%80%E5%9B%BE%E6%B5%81/)
